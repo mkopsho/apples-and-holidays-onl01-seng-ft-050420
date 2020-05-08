1a04df6b3aa84122a6d92ab908a091655a4c2772
Michael Kopsho <michael.kopsho@gmail.com>
2020-05-08 20:12:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,7 +50,10 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     puts season.to_s.capitalize + ":"
     holidays.each do |holiday, supplies|
-      puts holiday.to_s.split("_").join(" ").capitalize + ": " + supplies.join(", ")
+      if holiday == :fourth_of_july || holiday == :memorial_day || holiday == :new_years
+        puts holiday.to_s.split("_").join(" ").capitalize + ": " + supplies.join(", ")
+      else
+        puts holiday.to_s.split("_").join(" ").capitalize + ": " + supplies.join(", ")
     end
   end
 end
