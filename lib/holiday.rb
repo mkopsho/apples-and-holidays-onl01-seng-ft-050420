6f19795952d151a61e32e406ef4e06d2d4f709a7
Michael Kopsho <michael.kopsho@gmail.com>
2020-05-08 20:03:56 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,7 +50,9 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     puts season.to_s.capitalize + ":"
     holidays.each do |holiday, supplies|
-      puts 
+      puts holiday.to_s.capitalize + ":" + supplies.join(", ")
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
